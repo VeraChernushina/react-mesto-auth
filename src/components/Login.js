1d2ff1e42f93d6fc6e634cf495a3c3c0e41a62e1
Vera Chernushina <vera.chernushina@yandex.ru>
2022-02-04 16:46:32 +0300

    refactor Login styles

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,15 +3,8 @@ const Login = () => {
     <div className="login">
       <h2 className="login__title">Вход</h2>
       <form className="login__form">
-        <label>
-          Email
-          <input type="email" required />
-        </label>
-
-        <label>
-          Пароль
-          <input type="password" min="8" required />
-        </label>
+        <input type="email" placeholder="Email" required />
+        <input type="password" min="8" placeholder="Пароль" required />
         <button type="submit">Войти</button>
       </form>
     </div>
